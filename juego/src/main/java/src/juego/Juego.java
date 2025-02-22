33ca3991770dd81523686e21171976bae9fd4c02
Hugo <hugo.mesa1202@gmail.com>
2025-02-22 20:59:23 +0100

    crear contructor, setters y getters y el tostring de la clase juego, version inicial y unas pruebas en el main

--- juego/src/main/java/src/juego/Juego.java
+++ juego/src/main/java/src/juego/Juego.java
@@ -1,5 +1,76 @@
 package src.juego;
 
-public class Juego {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Juego {
+	private String ubicacion;
+	private List<Participantes> participantes;
+	private LocalDate fechaEvento;
+	private int numMinijuegos; //estoy tentado en hacer un enum pero de momento no lo voy a hacer
+	private int minijuegosRestantes;
+	private double boteTotal; // dinero acumulado segun la cantidad de eliminados
+	
+	public Juego(String ubicacion, LocalDate fechaEvento, int numMinijuegos, double boteInicial) {
+		this.participantes = new ArrayList<>();
+		this.ubicacion = ubicacion;
+		this.fechaEvento = fechaEvento;
+		this.numMinijuegos = numMinijuegos;
+		this.boteTotal = boteInicial;
+	}
+
+	public List<Participantes> getParticipantes() {
+		return participantes;
+	}
+
+	public void setParticipantes(List<Participantes> participantes) {
+		this.participantes = participantes;
+	}
+
+	public int getNumMinijuegos() {
+		return numMinijuegos;
+	}
+
+	public void setNumMinijuegos(int numMinijuegos) {
+		this.numMinijuegos = numMinijuegos;
+	}
+
+	public double getBoteTotal() {
+		return boteTotal;
+	}
+
+	public void setBoteTotal(double boteTotal) {
+		this.boteTotal = boteTotal;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public LocalDate getFechaEvento() {
+		return fechaEvento;
+	}
+	
+	public int getMinijuegosRestantes() {
+		return minijuegosRestantes;
+	}
+
+	public void setMinijuegosRestantes(int minijuegosRestantes) {
+		this.minijuegosRestantes = minijuegosRestantes;
+	}
+	
+	public void agregarParticipante(Participantes p) {
+		this.participantes.add(p);
+	}
+
+	@Override
+	public String toString() {
+		String info = "Ubicacion: " + this.ubicacion + "\n" +
+                "Fecha del evento: " + this.fechaEvento + "\n" +
+                "Numero de participantes: " + this.participantes.size() + "\n" +
+                "Bote total: " + this.boteTotal + "\n" +
+				"Numero de ronda: " + this.minijuegosRestantes + "/" + this.numMinijuegos + "\n";
+        return info;
+	}
 }
--- juego/src/main/java/src/juego/Main.java
+++ juego/src/main/java/src/juego/Main.java
@@ -4,12 +4,17 @@ import java.time.LocalDate;
 
 public class Main {
 	public static void main(String[] args) {
-		Participantes p1 = new Participantes( "paa", "peel", LocalDate.of(2000, 12, 28), "Ha", "Cakahuense", 700.7);
-		Participantes p2 = new Participantes( "paa", "peel", LocalDate.of(2000, 2, 29), "Ha", "Cakahuense", 700.7, "pepe");
-		Participantes p3 = new Participantes( "paa", "peel", LocalDate.of(2000, 2, 29), "Ha", "Cakahuense", 700.7, "pepe");
+		Juego game = new Juego("Oclajoma", LocalDate.of(2025, 2, 28), 10, 10000.11);
+		Participantes p1 = new Participantes("paa", "peel", LocalDate.of(2000, 12, 28), "Ha", "Cakahuense", 700.7);
+		Participantes p2 = new Participantes("paa", "peel", LocalDate.of(2000, 2, 29), "Ha", "Cakahuense", 700.7, "pepe");
+		Participantes p3 = new Participantes("paa", "peel", LocalDate.of(2000, 2, 29), "Ha", "Cakahuense", 700.7, "pepe");
 		//p3.eliminar();
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
+		game.agregarParticipante(p3);
+		game.agregarParticipante(p1);
+		game.agregarParticipante(p2);
+		System.out.println(game);
 	}
 }
