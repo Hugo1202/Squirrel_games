ce37da971486b46a4f57a5e3ea5a8dd77740d151
CristhianValerio <Cristhianvalerio2004@gmail.com>
2025-03-02 12:48:42 +0100

    Realizo la documentacion de la clase Prueba

--- juego/src/main/java/juego/Prueba.java
+++ juego/src/main/java/juego/Prueba.java
@@ -5,6 +5,13 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * La clase {@code Prueba} es la encargada de simular la ejecución de la prueba.
+ * 
+ * @version 1.0
+ * @see Juego
+ */
+
 public class Prueba {
 	private TipoPrueba tipo;
     private List<Participantes> inscritos;
@@ -12,6 +19,17 @@ public class Prueba {
     private List<Participantes> vencedores;
     //private Pink_guards responsable;
 
+    
+    /**
+     * Obtener información de los componentes de la prueba.
+     * 
+     * @param tipo, especifica el tipo de prueba que se realizará.
+     * @param inscritos, es una lista de los participantes que hay antes de iniciar la prueba.
+     * @param eliminados, es la lista de los participantes que han sido eliminados en la prueba.
+     * @param vencedores, es la lista de los participantes que han superado la prueba.
+     */
+    
+    
     public Prueba(TipoPrueba tipo) {
         this.tipo = tipo;
         this.inscritos = new ArrayList<>();
@@ -41,6 +59,24 @@ public class Prueba {
 		vencedores.sort(Comparator.comparingInt(Participantes::getId));
 		return vencedores;
 	}
+	
+	/**
+	 * Simula la ejecución de la prueba.
+	 * 
+	 * Este método toma una lista de participantes y simula una prueba, donde seran eliminados de manera aleatoria. 
+	 * 
+	 * El flujo es:
+	 * <ol>
+	 *   <li>Se registran los participantes como "inscritos".</li>
+	 *   <li>Se calcula el porcentaje de eliminados de forma aleatoria, dentro de un rango permitido.</li>
+	 *   <li>Se mezclan los inscritos (shuffle) para que la eliminación sea aleatoria.</li>
+	 *   <li>Se eliminan los participantes correspondientes al porcentaje de eliminados </li>
+	 *   <li>Los eliminados se añaden a la lista de eliminados.</li>
+	 *   <li>El resto de participantes pasan a la lista de vencedores.</li>
+	 * </ol>
+	 * 
+	 * @param participantes Lista de participantes inscritos en la prueba.
+	 */
 
 	public void simularPrueba(List<Participantes> participantes) {
 		setInscritos(participantes);
