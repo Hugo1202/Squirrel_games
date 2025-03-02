d06ba0c9ff471d5c24d39e892244388987eae6c7
CristhianValerio <Cristhianvalerio2004@gmail.com>
2025-03-02 12:49:24 +0100

    Realizo la documentaćión de la clase TipoPrueba

--- juego/src/main/java/juego/TipoPrueba.java
+++ juego/src/main/java/juego/TipoPrueba.java
@@ -1,5 +1,16 @@
 package juego;
 
+/**
+ * La clase {@code TipoPrueba} representa los tipos de pruebas que se realizaran
+ * en los juegos.
+ * 
+ * <p> Cada prueba tiene diferente probabilidad de muerte </p>
+ * 
+ * @version 1.0
+ * @see Prueba
+ * 
+ */
+
 public enum TipoPrueba {
 	LUZ_ROJA_LUZ_VERDE("Luz verde luz roja",  "Correr cuando la muñeca no mire.", 30, 65),
     DALGONA("Dalgona", "Cortar la galleta sin romperla.", 30, 55),
@@ -9,6 +20,17 @@ public enum TipoPrueba {
     private final String descripcion;
     private final int minEliminados;
     private final int maxEliminados;
+    
+    
+    
+    /**
+     * Obtener información de las pruebas.
+     * 
+     * @param nombre de la prueba.
+     * @param descripcion de la prueba.
+     * @param minEliminados, es la cantidad minima de participantes eliminados que debe de tener la prueba.
+     * @param maxEliminados, es la cantidad maxima de participantes eliminados que debe de tener la prueba.
+     */
     
     TipoPrueba(String nombre, String descripcion, int minEliminados, int maxEliminados) {
     	this.nombre = nombre;
