b8b485bdcba9ef40d1f6505ce8dba00b99243dca
CristhianValerio <Cristhianvalerio2004@gmail.com>
2025-03-02 12:45:42 +0100

    Realizo la documentación de la clase Participantes

--- juego/src/main/java/juego/Participantes.java
+++ juego/src/main/java/juego/Participantes.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 
 import excepciones.InfiltradoNoEliminableException;
 
+/**
+ * La clase {@code Participantes} es una clase hija de la clase {@code Persona}.Se utiliza para poder 
+ * utilizar la estructura básica de las personas.
+ * 
+ * @version 1.0
+ * @see Persona
+ */
 
 public class Participantes extends Persona {
     private int id;
@@ -14,6 +21,20 @@ public class Participantes extends Persona {
     private static int siguienteId = 1;
 
 
+    /**
+     * Obtener información de los participantes no infiltrados.
+     * 
+     * @param nombre del participante.
+     * @param apellidos del participante.
+     * @param fechaNacimiento del participante.
+     * @param sexo del participante.
+     * @param nacionalidad del participante.
+     * @param deuda acumulada del participante.
+     * @param infiltrado, al estar en false nos indica que el participante no es infiltrado.
+     * @param eliminado, nos indica si el participante esta eliminado.
+     * @param id, nos asegura que nos da un ID unico para cada participante.
+     */
+    
     public Participantes(String nombre, String apellidos, LocalDate fechaNacimiento, String sexo, String nacionalidad, double deudaAcumulada) {
         super(nombre, apellidos, fechaNacimiento, sexo, nacionalidad);
         this.deudaAcumulada = deudaAcumulada;
@@ -22,6 +43,21 @@ public class Participantes extends Persona {
         this.id = siguienteId++; 
     }
 
+    
+    /**
+     * Obtener información de los participantes infiltrados.
+     * 
+     * @param nombre del infiltrado.
+     * @param apellidos del infiltrado.
+     * @param fechaNacimiento del infiltrado.
+     * @param sexo del infiltrado.
+     * @param nacionalidad del infiltrado.
+     * @param deuda acumulada del infiltrado.
+     * @param infiltrado, al estar en true nos indica que es infiltrado.
+     * @param nombreFalso del infiltrado.
+     * @param eliminado, nos indica si el participante esta eliminado.
+     * @param id, nos asegura que nos da un ID unico para cada infiltrado.
+     */
 
     public Participantes(String nombre, String apellidos, LocalDate fechaNacimiento, String sexo, String nacionalidad, double deudaAcumulada, String nombreFalso) {
         super(nombre, apellidos, fechaNacimiento, sexo, nacionalidad);
@@ -74,7 +110,14 @@ public class Participantes extends Persona {
         this.eliminado = eliminado;
     }
 
-
+/**
+ * Elimina a participantes.
+ * 
+ * 
+ * @throws InfiltradoNoEliminableException si el participante es infiltrado.
+ */
+    
+    
     public void eliminar() throws InfiltradoNoEliminableException {
 //        if (infiltrado) {
 //            throw new InfiltradoNoEliminableException("No se puede eliminar a un jugador infiltrado.");
