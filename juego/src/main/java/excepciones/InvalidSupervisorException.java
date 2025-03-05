42f6d7389fcb02b520614437134e081c72da6292
adamupe <adamupedam@gmail.com>
2025-03-05 19:18:53 +0100

    Añado excepciones InvalidSupervisorException y OrganizationException

---
+++ juego/src/main/java/excepciones/InvalidSupervisorException.java
@@ -0,0 +1,10 @@
+package squirrelGames;
+
+public class InvalidSupervisorException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidSupervisorException(String message) {
+        super(message);
+    }
+
+}
---
+++ juego/src/main/java/excepciones/OrganizationException.java
@@ -0,0 +1,10 @@
+package squirrelGames;
+
+public class OrganizationException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public OrganizationException(String message) {
+        super(message);
+    }
+
+}
