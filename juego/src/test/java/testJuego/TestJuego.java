bbf07e55bb128b1df80189f8956a03980b48058a
Hugo <hugo.mesa1202@gmail.com>
2025-03-07 20:48:55 +0100

    Limpieza 3

--- juego/src/test/java/testJuego/TestJuego.java
+++ juego/src/test/java/testJuego/TestJuego.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import enums.*;
+import enums.WeaponType;
+import enums.Department;
 import excepciones.*;
 import juego.*;
 
