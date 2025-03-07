47354711b83fd6c14365eede09478fde2d2a35f0
adamupe <adamupedam@gmail.com>
2025-03-07 18:52:33 +0100

    Corrección PinkGuards y JavaDoc

--- juego/src/main/java/juego/PinkGuards.java
+++ juego/src/main/java/juego/PinkGuards.java
@@ -1,23 +1,39 @@
 package juego;
 
-
+/**
+ * Clase abstracta miembro organización PinkGuards.
+ */
 public abstract class PinkGuards {
 	protected String name;
 
+	/**
+	 * Constructor PinkGuard con nombre:
+	 * @param name nombre miembro
+	 */
 	public PinkGuards(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Devuelve nombre mimebro:
+	 * @return nombre
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Establece nuevo nombre:
+	 * @param name nuevo nombre
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	
-	
+	/**
+	 * Devuelve rango del miembro:
+	 * @return rango
+	 */
 	public abstract int getRank();
 	
 	@Override
